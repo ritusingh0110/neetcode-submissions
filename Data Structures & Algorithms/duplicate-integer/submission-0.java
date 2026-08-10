a0b6067b5518da258d746fc36468cb2c8bc3d107
Ritu Singh <ritu10d.21@gmail.com>
2026-08-11 02:01:09 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int ele:nums){
+            if(set.contains(ele))return true;
+            set.add(ele);
+        }
+        return false;
+    }
+}
